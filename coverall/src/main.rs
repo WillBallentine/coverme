c4209e857f548647d8aa6f92c72302fd04648bf5
WillBallentine <willh.ballentine@gmail.com>
2025-02-14 16:50:01 -0500

    initial progress bar

--- coverall/src/main.rs
+++ coverall/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use anyhow::{Context, Result};
+use indicatif::ProgressBar;
 
 #[derive(Parser)]
 struct Cli {
@@ -9,15 +10,20 @@ struct Cli {
 
 fn main() -> Result<()>{
     let args = Cli::parse();
+    let pb = ProgressBar::new(100);
 
     let content = std::fs::read_to_string(&args.repo)
         .with_context(|| format!("Error reading `{}`", args.repo.display()))?;
 
+    let mut iter = 1;
     for line in content.lines() {
+        pb.println(format!("[+] reading line  #{}", iter));
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
+        iter += 1;
     }
+    pb.finish_with_message("done");
 
     Ok(())
 }
