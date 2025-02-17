5bf6c57d5d282ec118b8eb6d137371dca2bd88fe
willballentine <willh.ballentine@gmail.com>
2025-02-17 15:12:20 -0500

    refactor

--- coverall/src/main.rs
+++ coverall/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::result::Result::Ok;
 
+use clap::{Arg, ArgMatches};
 use cli::get_cli_args;
 
 use anyhow::{Context, Result};
@@ -8,23 +9,35 @@ use colored::*;
 
 pub mod cli;
 
+struct Command {
+    repo: String,
+    pattern: String,
+}
+
 fn run() -> Result<()> {
     let cli_args = get_cli_args(env::args_os());
-    let repo = cli_args.get_one::<String>("repo").unwrap();
-    let pattern = cli_args.get_one::<String>("pattern").unwrap();
 
-    let content = std::fs::read_to_string(cli_args.get_one::<String>("repo").unwrap())
-        .with_context(|| format!("Error reading `{}`", repo.red()))?;
+    let command = unwrap_command(cli_args);
+
+    let content =
+        std::fs::read_to_string(command.repo).with_context(|| format!("Error reading repo"))?;
 
     let mut iter = 1;
     for line in content.lines() {
-        if line.contains(pattern) {
+        if line.contains(command.pattern.as_str()) {
             println!("found on line [{}]: {}", iter, line);
         }
         iter += 1;
     }
 
     Ok(())
+}
+
+fn unwrap_command(cli_args: ArgMatches) -> Command {
+    Command {
+        repo: cli_args.get_one::<String>("repo").unwrap().clone(),
+        pattern: cli_args.get_one::<String>("pattern").unwrap().clone(),
+    }
 }
 
 fn main() {
